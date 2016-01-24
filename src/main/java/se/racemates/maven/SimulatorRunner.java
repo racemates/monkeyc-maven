1126422706f157b9b9c18b0f2b14415d849f36bf
Johan Granlund <johan.granlund@primamkod.se>
2016-01-24 22:51:10 +0100

    dc

--- src/main/java/se/racemates/maven/SimulatorRunner.java
+++ src/main/java/se/racemates/maven/SimulatorRunner.java
@@ -3,6 +3,7 @@ package se.racemates.maven;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
 
 public class SimulatorRunner {
 
@@ -31,12 +32,13 @@ public class SimulatorRunner {
             for (int port = 1234; port < 1239; port++) {
                 transferFile = pushProgram(port, sdkPath, programFile);
                 transferFile.waitFor();
-                if (transferFile.exitValue() == 1) {
+                System.out.println("Exit value:" + transferFile.exitValue() );
+                if (transferFile.exitValue() == 0) {
                     programProcess = startProgram(port, sdkPath, programFile);
                     break;
                 }
             }
-            if (transferFile.exitValue() == 1) {
+            if (transferFile.exitValue() == 0) {
                 break;
             }
             Thread.sleep(500);
@@ -45,9 +47,7 @@ public class SimulatorRunner {
         assert programProcess != null;
 
         final StreamGobbler errorGobbler = new StreamGobbler(programProcess.getErrorStream(), System.err);
-        errorGobbler.start();
-
-        final Process finalProgramProcess = programProcess;
+ ;      final Process finalProgramProcess = programProcess;
 
         new Thread() {
             @Override
@@ -59,14 +59,15 @@ public class SimulatorRunner {
                 }
             }
         }.start();
+        errorGobbler.start();
 
-
-        final InputStream inputStream = programProcess.getInputStream();
-
-        return inputStream;
+        return programProcess.getInputStream();
     }
 
     private static Process pushProgram(int port, String sdkPath, String programFile) throws IOException {
+        final String name = new File(programFile).getName();
+        System.out.println("pushProgram port:" + port);
+
         return
                 new ProcessBuilder().
                         inheritIO().
@@ -76,12 +77,14 @@ public class SimulatorRunner {
                                 "--transport_args=127.0.0.1:" + port,
                                 "push",
                                 "\"" + programFile + "\"",
-                                "0:/GARMIN/APPS/$FILE"
+                                "0:/GARMIN/APPS/" + name
                         ).start();
 
     }
 
-    private static Process startProgram(int port, String sdkPath, String programFile) throws IOException {
+    private static Process startProgram(final int port, final String sdkPath, final String programFile) throws IOException {
+        final String name = new File(programFile).getName();
+        System.out.println("name:" + name + " port:" + port);
         return
                 new ProcessBuilder().
                         command(
@@ -89,18 +92,9 @@ public class SimulatorRunner {
                                 "--transport=tcp",
                                 "--transport_args=127.0.0.1:" + port,
                                 "tvm",
-                                "\"" + programFile + "\"",
-                                "0:/GARMIN/APPS/" + new File(programFile).getName(),
+                                "0:/GARMIN/APPS/" + name,
                                 "1"
                         ).start();
 
     }
-
-
-    //"$MB_HOME"/shell --transport=tcp --transport_args=127.0.0.1:$i push "$PRG_PATH" "0:/GARMIN/APPS/$FILE"
-    //        if [ $? -eq 1 ]; then
-    //continue
-    //fi
-    //"$MB_HOME"/shell --transport=tcp --transport_args=127.0.0.1:$i tvm "0:/GARMIN/APPS/$FILE" $DEVICE_ID
-
 }
