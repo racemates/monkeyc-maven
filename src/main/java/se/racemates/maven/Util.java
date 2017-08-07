ef97197fdfc3351f8c9f3148c3c416cca2a9cf1c
Johan Granlund <johan.granlund@racemates.e>
2017-08-07 18:13:52 +0200

    removed unused code

--- src/main/java/se/racemates/maven/Util.java
+++ src/main/java/se/racemates/maven/Util.java
@@ -1,30 +1,11 @@
 package se.racemates.maven;
 
-public class Util {
+public final class Util {
 
     private static final String os = System.getProperty("os.name").toLowerCase();
 
     public static boolean isWindows() {
         return os.contains("win");
-    }
-
-    @SuppressWarnings("unused")
-    public static boolean isMac() {
-        return os.contains("mac");
-    }
-
-    @SuppressWarnings("unused")
-    public static boolean isUnix() {
-        return  os.contains("nix") ||
-                os.contains("nux") ||
-                os.contains("aix");
-    }
-
-    public static String platformCommand(
-            final String path,
-            final String file
-    ) {
-        return path + "/" + file + (isWindows() ? ".exe" : "");
     }
 
 
